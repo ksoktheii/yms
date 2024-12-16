34fad414421793d4cbeb2ebd847ab8424baed8d0
Chaser-254 <esibitaremmanuel316@gmail.com>
2024-12-16 12:25:05 +0300

    backend

---
+++ yms/youth/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.sign_up, name='user_sign'),
+    path('dashboard', views.user_dashboard, name='user_dashboard'),
+]
--- yms/youth/views.py
+++ yms/youth/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
 
-# Create your views here.
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return HttpResponse('A new user has been successfully registered!')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def user_dashboard(request):
+    return render(request, 'dashboard.html')
